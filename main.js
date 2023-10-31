136988cb6c66978020f9c51bd9b7bd832b73b5b7
Narges-Danesh <133409792+Narges-Danesh@users.noreply.github.com>
2023-10-31 18:27:48 +0330

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const navbar = document.getElementById("navbar");
 const locationTags = document.getElementById("location-tags");
 const popularSection = document.getElementById("popular-places");
+const recommendedSection = document.getElementById("recommended");
+// ===================== NAV SCROLL EFFECT ==================
 addEventListener("scroll", () => {
   if (window.scrollY > 0) {
     navbar.classList.add("shadow");
@@ -8,7 +10,7 @@ addEventListener("scroll", () => {
     navbar.classList.remove("shadow");
   }
 });
-
+// ===================== LOCATION TAGS ===================
 const locations = [
   "انگلیس",
   "فرانسه",
@@ -27,8 +29,8 @@ locations.forEach((tag) => {
   span.appendChild(aTag);
   locationTags.appendChild(span);
 });
-
-const locationCards = [
+// ===================== POPULAR LOCATIONS ===================
+const popularLocations = [
   {
     img: "morocco/morocco-cover.jpg",
     location: "مراکش",
@@ -45,8 +47,7 @@ const locationCards = [
     stars: 4.7,
   },
 ];
-
-locationCards.forEach((card) => {
+popularLocations.forEach((card) => {
   const { img, location, stars } = card;
 
   popularSection.innerHTML += `
@@ -57,5 +58,55 @@ locationCards.forEach((card) => {
               <span class="material-symbols-outlined star-icon"> star </span>
             </div>
           </div>
+`;
+});
+// ===================== RECOMMENDED LOCATIONS ===================
+const recommendedLocations = [
+  {
+    img: "dubai/dubai-cover.jpg",
+    location: "دبی",
+    stars: 4.9,
+    date: "1 بهمن تا 25 بهمن",
+    days: 25,
+  },
+  {
+    img: "georgia/georgia-cover.jpg",
+    location: "گرجستان",
+    stars: 4.6,
+    date: "1 اسفند تا 10 اسفند",
+    days: 10,
+  },
+  {
+    img: "japan/japan-cover.webp",
+    location: "ژاپن",
+    stars: 4.7,
+    date: "1 دی تا 12 دی",
+    days: 12,
+  },
+];
+
+recommendedLocations.forEach((card) => {
+  const { img, location, date, days, stars } = card;
+
+  recommendedSection.innerHTML += `
+<div class="recommended-card" >
+<div class="location-cover" style=' background-image: url(images/${img});'>
+</div>
+<div class="desc">
+  <div class="name">${location}</div>
+  <div class="time">
+    <span class="material-symbols-outlined">today</span>
+    <span>${date}</span>
+  </div>
+  <div class="duration">
+    <span class="material-symbols-outlined">watch</span>
+    ${days} <small>روز</small>
+  </div>
+</div>
+<div class="stars">
+  <span>${stars}</span>
+  <span class="material-symbols-outlined star-icon"> star </span>
+</div>
+</div>
 `;
 });
