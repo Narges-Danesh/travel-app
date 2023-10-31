3a11940c2e739a9eb32ccdb007e6d879a83ab822
Narges-Danesh <133409792+Narges-Danesh@users.noreply.github.com>
2023-10-31 17:36:51 +0330

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const navbar = document.getElementById("navbar");
 const locationTags = document.getElementById("location-tags");
+const popularSection = document.getElementById("popular-places");
 addEventListener("scroll", () => {
   if (window.scrollY > 0) {
     navbar.classList.add("shadow");
@@ -18,11 +19,43 @@ const locations = [
   "ارمنستان",
   "دبی",
 ];
-locations.forEach(tag => {
-    const span = document.createElement('span');
-    span.classList.add('tag');
-    const aTag = document.createElement('a');
-    aTag.innerHTML = tag;
-    span.appendChild(aTag)
-    locationTags.appendChild(span)
-})
+locations.forEach((tag) => {
+  const span = document.createElement("span");
+  span.classList.add("tag");
+  const aTag = document.createElement("a");
+  aTag.innerHTML = tag;
+  span.appendChild(aTag);
+  locationTags.appendChild(span);
+});
+
+const locationCards = [
+  {
+    img: "morocco/morocco-cover.jpg",
+    location: "مراکش",
+    stars: 4.6,
+  },
+  {
+    img: "nepal/nepal-cover.jpg",
+    location: "نپال",
+    stars: 4.2,
+  },
+  {
+    img: "india/india-cover.jpg",
+    location: "هند",
+    stars: 4.7,
+  },
+];
+
+locationCards.forEach((card) => {
+  const { img, location, stars } = card;
+
+  popularSection.innerHTML += `
+<div class="popular-card" style='background-image: url(images/${img}), linear-gradient(180deg, transparent, black)'>
+            <div class="card-location-name">${location}</div>
+            <div class="stars">
+              <span>${stars}</span>
+              <span class="material-symbols-outlined star-icon"> star </span>
+            </div>
+          </div>
+`;
+});
